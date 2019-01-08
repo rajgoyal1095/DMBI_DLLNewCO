8ee2953efb49116e9f57ed452f0a6560e32de2dd
rajgoyal1095 <45995952+rajgoyal1095@users.noreply.github.com>
2019-01-08 10:12:17 +0530

    Change the file
    Modify this file

---
+++ src/MyFirst.java
@@ -0,0 +1,29 @@
+public class MyFirst {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+        Add a=new Add();
+        a.add();
+        
+    }
+}
+class Add{
+    public void add()
+    {
+        int a=20,b=40,c;
+        c=a+b;
+        system.out.println("Addition of a and b :"+c);
+    }
+    public void Multi(){
+        int a=30,b=40,c;
+        c=a*b;
+        system.out.println("Multiply by a and b :" +c);
+
+}
+    public void Sub(){
+        int a=50,b=40,c;
+        c=a-b;
+        system.out.println("Subtract a and b :" +c);
+     }
+
+}
+
